da4a4bceabdf450e8ef94037119f16806652129f
lara personal <lara.bautista16@gmail.com>
2025-05-13 10:55:28 +0200

    make it responsive

--- lib/blue_mana_track.dart
+++ lib/blue_mana_track.dart
@@ -9,7 +9,7 @@ class BlueManaTrack extends StatefulWidget {
 
 class _BlueManaTrackState extends State<BlueManaTrack> {
   int _counter = 0;
-  List<int> sumInts = [1, 2, 4, 7, 10, 16];
+  final List<int> sumInts = [1, 2, 4, 7, 10, 16];
 
   void _incrementCounter(int element) {
     setState(() {
@@ -25,44 +25,67 @@ class _BlueManaTrackState extends State<BlueManaTrack> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool isSmallScreen = screenWidth < 500;
+
+    // Botones de restar
+    final decreaseButtons = sumInts.reversed.map(
+      (element) => TextButton(
+        onPressed: () => _decreaseCounter(element),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [Text(element.toString()), const Icon(Icons.remove)],
+        ),
+      ),
+    );
+
+    // Botones de sumar
+    final increaseButtons = sumInts.map(
+      (element) => TextButton(
+        onPressed: () => _incrementCounter(element),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [Text(element.toString()), const Icon(Icons.add)],
+        ),
+      ),
+    );
+
+    final counterWidget = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Text(
+        '$_counter',
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+    );
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
+        const Text(
           'Blue mana tracker',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ...sumInts.reversed.map(
-              (element) => TextButton(
-                onPressed: () => _decreaseCounter(element),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(element.toString()),
-                    const Icon(Icons.remove),
-                  ],
+        const SizedBox(height: 8),
+        isSmallScreen
+            ? Wrap(
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              spacing: 2,
+              runSpacing: 2,
+              children: [
+                ...decreaseButtons,
+                Row(
+                  children: [counterWidget],
+                  mainAxisAlignment: MainAxisAlignment.center,
                 ),
-              ),
+                ...increaseButtons,
+              ],
+            )
+            : Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [...decreaseButtons, counterWidget, ...increaseButtons],
             ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            ...sumInts.map(
-              (element) => TextButton(
-                onPressed: () => _incrementCounter(element),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [Text(element.toString()), const Icon(Icons.add)],
-                ),
-              ),
-            ),
-          ],
-        ),
       ],
     );
   }
